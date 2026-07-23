0d54dc23912378d217a071acc2cd88451b26144e
P-lauski <plauski@tuta.io>
2026-07-23 16:58:37 -0400

    feat(mlo): Add mlo files to repo

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Imperial Studio'
+description 'Power St Records'
+version '1.0.2'
+
+this_is_a_map 'yes'
+
+-- Audio Occlusions - Comment out the lines below if you wish to disable it.
+files {
+    'audio/*.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/7B87D75B_game.dat' -- dat151
